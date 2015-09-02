a0c6fccd0ea1c085c7e832728ff412f1adb69885
Matt Lo <mattlo.developer@gmail.com>
2015-09-02 18:13:22 -0500

    created date scaffolder

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 var app = angular.module('dinner', []);
+
+app.controller('scheduler', function ($scope) {
+    $scope.days = [];
+
+    // scaffold out the next 3 months
+    $scope.scaffold = function (calendarScope) {
+        return Array(parseInt(calendarScope, 10)).join(0).split(0).reduce(function (arr, item, i) {
+            arr.push(moment(arr[i]).add(1, 'days'));
+            return arr;
+        }, [moment()]);
+    }
+});
