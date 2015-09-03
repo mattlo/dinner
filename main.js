a4199977464e1449f81fbebd057d2b2e5a92068f
Matt Lo <mattlo.developer@gmail.com>
2015-09-02 19:32:11 -0500

    added add/remove functionality, added availability checker, fixed reference check

--- main.js
+++ main.js
@@ -4,16 +4,19 @@ var app = angular.module('dinner', [
 
 app.controller('scheduler', function ($scope, $firebaseObject) {
     var ref = new Firebase('https://mattlo.firebaseio.com');
+    var syncObject = $firebaseObject(ref);
     var DELIMITER = '|';
 
     $scope.days = [];
     $scope.name;
     $scope.rawName;
     $scope.forceForm = false;
-    $scope.data = $firebaseObject(ref);
+
+    // 3-way binding init
+    syncObject.$bindTo($scope, 'data');
 
     /**
-     * Scaffold out the next 3 months
+     * Scaffold out the next N of days
      * @param calendarScope
      * @returns {Array}
      */
@@ -40,11 +43,40 @@ app.controller('scheduler', function ($scope, $firebaseObject) {
 
     /**
      * is user available?
+     * @param {moment} day
      * @param {String} userData
      * @returns {Boolean}
      */
-    $scope.isAvailable = function (userData) {
-        return true;
+    $scope.isAvailable = function (day, userData) {
+        if (!userData) {
+            return true;
+        }
+
+        return userData.indexOf(day.format('L')) < 0;
+    };
+
+    /**
+     * append
+     * @param {string} name
+     * @param {moment} day
+     * @return {undefined}
+     */
+    $scope.book = function (name, day) {
+        if (!$scope.data.attendees) {
+            $scope.data.attendees = {};
+        }
+
+        $scope.data.attendees[name] += DELIMITER + day.format('L');
+    };
+
+    /**
+     * remove
+     * @param {string} name
+     * @param {moment} day
+     * @return {undefined}
+     */
+    $scope.unbook = function (name, day) {
+        $scope.data.attendees[name] = $scope.data.attendees[name].replace(DELIMITER + day.format('L'), '');
     };
 
     /**
@@ -52,22 +84,21 @@ app.controller('scheduler', function ($scope, $firebaseObject) {
      * @return {String}
      */
     $scope.outputAvailableNames = function (day) {
-        return ((function () {
-            if ($scope.data.attendees) {
+        if ($scope.data && $scope.data.attendees) {
+            return ((function () {
                 return Object.keys($scope.data.attendees).reduce(function (arr, name) {
                     // verify item exists
                     if (
                         $scope.data.attendees[name].indexOf(day.format('L')) >= 0 &&
-                        arr.indexOf(name) < 0 &&
-                        name === $scope.name
+                        arr.indexOf(name) < 0
                     ) {
                         arr.push(name);
                     }
 
                     return arr;
                 }, []);
-            }
-        }()) || []).join(', ');
+            }()) || []).join(', ');
+        }
     };
 
     // get cached name
